caa6c36bf9379735166e855ab8a3cb588cced633
yondav <yoni@yondav.us>
2023-06-12 02:20:44 -0400

    feat(hooks): adds copy to clipboard hook

--- website/src/hooks/index.ts
+++ website/src/hooks/index.ts
@@ -1,6 +1,7 @@
+export { default as useCopyToClipboard } from './useCopyToClipboard';
 export { default as useEventListener } from './useEventListener';
 export { default as useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 export { default as useMediaQuery } from './useMediaQuery';
-export { default as useMethodList } from './useMethodList';
+export { default as useExportList } from './useExportList';
 export { default as useOnClickOutside } from './useOnClickOutside';
 export { default as useSiteMetadata } from './useSiteMetadata';
