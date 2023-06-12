4ddccad61461b2277802f82f5e2928b36e02a14a
yondav <yoni@yondav.us>
2023-06-12 02:03:26 -0400

    chore(components): removes layout components

--- website/src/components/layout/styled.ts
+++
@@ -1,9 +0,0 @@
-import tw, { styled } from 'twin.macro';
-
-const BarContainer = styled.div<{ bg?: boolean }>(({ bg }) => ([ tw`flex justify-between items-center p-5`, bg && tw`bg-bg` ]));
-
-const IconButton = styled.button.attrs({ type: 'button' })(tw`text-neutral-800 hover:text-fg transition`);
-
-const LinkContainer = styled.div(tw`flex items-center gap-1 mt-auto`);
-
-export { BarContainer, IconButton, LinkContainer };
