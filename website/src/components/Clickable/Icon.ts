57e2cdc9bcaa8e612da34f1f310b672f28718306
yondav <yoni@yondav.us>
2023-06-12 02:12:52 -0400

    refactor(components): extracts icon button component to clickable components dir

---
+++ website/src/components/Clickable/Icon.ts
@@ -0,0 +1,5 @@
+import tw, { styled } from 'twin.macro';
+
+const IconWrapper = styled.button.attrs({ type: 'button' })(tw`text-neutral-800 hover:text-fg transition`);
+
+export default IconWrapper;
