4d61362bc303731da648e2a1b00ddc30a1d4c74d
yondav <yoni@yondav.us>
2023-06-12 01:56:59 -0400

    refactor: exports revised file structure

--- src/string/index.ts
+++ src/string/index.ts
@@ -1,2 +1,1 @@
-export { default as toCamelCase } from './toCamelCase/toCamelCase';
-export { default as toSlug } from './toSlug/toSlug';
+export * from './methods';
---
+++ src/string/methods/index.ts
@@ -0,0 +1,2 @@
+export { default as toCamelCase } from './toCamelCase/toCamelCase.source';
+export { default as toSlug } from './toSlug/toSlug.source';
