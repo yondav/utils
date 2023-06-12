e7f38fb4653644a1f5037a8623a5922aa715a923
yondav <yoni@yondav.us>
2023-06-12 02:20:11 -0400

    refactor(gatsby): updates createPages to create bothe template page types

--- website/src/gatsby/createPages.ts
+++ website/src/gatsby/createPages.ts
@@ -1,7 +1,10 @@
 import type { GatsbyNode } from 'gatsby';
 
-import createMethods from './createPages/createMethods';
+import createFunctionPages from './createPages/createFunctionPages';
+import createTypePages from './createPages/createTypePages';
 
 export const createPages: GatsbyNode['createPages'] = async args => {
-  await Promise.all([ createMethods(args) ]);
+  // await Promise.all([ createFunctionPages(args) ]);
+  await createFunctionPages(args);
+  await createTypePages(args);
 };
